89d4afd386153eaa80cbe50ef45f255c061ded68
Hangsha <codehangsha@gmail.com>
2023-08-20 00:27:54 +0530

    Add my code

---
+++ server.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// MongoDB Atlas connection URI
+const dbURI = 'mongodb+srv://root:root@cluster0.untdeh4.mongodb.net/?retryWrites=true&w=majority';
+
+// Connect to MongoDB Atlas
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB Atlas');
+});
+
+const itemSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+  res.redirect('/items');
+});
+
+app.post('/items', async (req, res) => {
+  try {
+    const newItem = new Item(req.body);
+    const savedItem = await newItem.save();
+    res.redirect('/items');
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get('/items', async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.render('index', { items });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/items/:id/edit', async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    if (!item) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+    res.render('edit', { item });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put('/items/:id', async (req, res) => {
+    try {
+      const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      if (!updatedItem) {
+        return res.status(404).json({ message: 'Item not found' });
+      }
+      res.redirect('/items'); // Redirect to the items list after successful update
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
+
+app.delete('/items/:id', async (req, res) => {
+  try {
+    const deletedItem = await Item.findByIdAndRemove(req.params.id);
+    if (!deletedItem) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+    res.redirect('/items');
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
